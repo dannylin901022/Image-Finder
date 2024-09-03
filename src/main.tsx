caffa0c7a4d2b45c9823546a92f165db4cb43524
dannylin901022 <dannylin901022@gmail.com>
2024-09-04 02:45:11 +0800

    solve top level can use await problem

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,14 @@ import './index.css'
 
 import { config } from './component/configAPI.tsx'
 
-await config.setNewHibiAPI();
+async function startApp(){
+  await config.setNewHibiAPI();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+}
+
+startApp();
